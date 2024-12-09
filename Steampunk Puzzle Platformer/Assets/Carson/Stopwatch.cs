302a5dd529da4dd4d7c8af1e53ce9616edcb65fa
Carson <carsonjmaurer@gmail.com>
2024-12-09 09:17:51 -0800

    finished stopwatch

--- Steampunk Puzzle Platformer/Assets/Carson/Stopwatch.cs
+++ Steampunk Puzzle Platformer/Assets/Carson/Stopwatch.cs
@@ -1,45 +1,95 @@
 using System;
+
 using System.Collections;
+
 using System.Collections.Generic;
+
 using TMPro;
+
 using Unity.Mathematics;
+
 using UnityEngine;
+
 using UnityEngine.UI;
 
 
+
+
+
 public class Stopwatch : MonoBehaviour
+
 {
-    public bool stopwatchActive = false;
-    float currentTime;
 
-    int score;
-    public TextMeshProUGUI currentTimeText;
-    public TextMeshProUGUI scoreText;
+public bool stopwatchActive = false;
 
-    public float mutiplier = 5;
-    // Start is called before the first frame update
-    void Start()
-    {
-        currentTime = 0;
-    }
+float currentTime;
 
-    // Update is called once per frame
-    void Update()
-    {
-        if (stopwatchActive == true) {
-            currentTime = currentTime + Time.deltaTime;
-        }
-        score = Mathf.RoundToInt(currentTime * mutiplier);
-        scoreText.text = score.ToString();
-        TimeSpan time = TimeSpan.FromSeconds(currentTime);
-        currentTimeText.text = time.ToString(@"mm\:ss\:");
-    }
 
-    public void StartStopwatch(){
-        stopwatchActive = true;
-    }
 
-    public void StopStopwatch(){
-        stopwatchActive = false;
-    }
+
+
+public TextMeshProUGUI currentTimeText;
+
+
+
+
+
+
+
+public float mutiplier = 2;
+
+// Start is called before the first frame update
+
+void Start()
+
+{
+
+currentTime = 0;
+
 }
+
+
+
+// Update is called once per frame
+
+void Update()
+
+{
+
+if (stopwatchActive == true) {
+
+currentTime = currentTime + Time.deltaTime;
+
+}
+
+
+
+TimeSpan time = TimeSpan.FromSeconds(currentTime);
+
+currentTimeText.text = time.ToString(@"mm\:ss");
+
+
+
+
+
+}
+
+
+
+public void StartStopwatch(){
+
+stopwatchActive = true;
+
+}
+
+
+
+public void StopStopwatch(){
+
+stopwatchActive = false;
+
+}
+
+}
+
+
